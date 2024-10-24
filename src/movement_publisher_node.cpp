11a44e439b2c04e5041fac03fc97801e16779781
minh_le <83758821+brianle169@users.noreply.github.com>
2024-10-23 22:03:28 -0400

    Add thread header file & instantiate a thread object for user data input

--- src/movement_publisher_node.cpp
+++ src/movement_publisher_node.cpp
@@ -1,4 +1,5 @@
 #include <chrono>
+#include <thread>
 #include "rclcpp/rclcpp.hpp"
 #include "custom_interfaces/msg/circle_movement.hpp"
 
@@ -6,21 +7,27 @@ class MovementPublisher : public rclcpp::Node {
 public:
     MovementPublisher() : Node("movement_publisher"), lx(2.5), az(1.5)
     {
+        RCLCPP_INFO(this->get_logger(), "Publisher Node is instantiated.");
         publisher_ = this->create_publisher<custom_interfaces::msg::CircleMovement>("movement_data", 10);
         timer_ = this->create_wall_timer(std::chrono::milliseconds(500), std::bind(&MovementPublisher::publishCallback, this));
+        input_thread_ = std::thread(std::bind(&MovementPublisher::updateMovementData,this));
     }
 
 private:
+    void updateMovementData() {
+
+    }
     void publishCallback() {
         // Instantiate and assign message
         auto message = custom_interfaces::msg::CircleMovement();
         message.linear_x = lx;
         message.angular_z = az;
-        RCLCPP_INFO(this->get_logger(), "Publishing movement data:\n- linear_x: %.1f\n- angular_z: %.1f",message.linear_x, message.angular_z);
+        // RCLCPP_INFO(this->get_logger(), "Publishing movement data:\n- linear_x: %.1f\n- angular_z: %.1f",message.linear_x, message.angular_z);
         publisher_->publish(message);
     }
     rclcpp::Publisher<custom_interfaces::msg::CircleMovement>::SharedPtr publisher_;
     rclcpp::TimerBase::SharedPtr timer_;
+    std::thread input_thread_;
     float lx, az; // linear-x and angular-z
 };
 
