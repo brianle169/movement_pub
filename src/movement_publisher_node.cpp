ff7676d46cb41693fcfd5721538badfa698babf3
minh_le <83758821+brianle169@users.noreply.github.com>
2024-10-23 21:37:36 -0400

    Add custom_interfaces/msg/CircleMovement dependency to the package & update config

--- src/movement_publisher_node.cpp
+++ src/movement_publisher_node.cpp
@@ -1,4 +1,16 @@
 #include "rclcpp/rclcpp.hpp"
+#include "custom_interfaces/msg/circle_movement.hpp"
+
+class MovementPublisher : public rclcpp::Node {
+public:
+    MovementPublisher() : Node("movement_publisher")
+    {
+        publisher_ = this->create_publisher<custom_interfaces::msg::CircleMovement>("movement_data", 10);
+    }
+
+private:
+    rclcpp::Publisher<custom_interfaces::msg::CircleMovement>::SharedPtr publisher_;
+};
 
 int main(int argc, char ** argv) {
     rclcpp::init(argc, argv);
